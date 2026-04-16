4982fbe80db0a9b73bf99648ed9fe000d63e7f89
Anjali Bhardwaj <anjalibhardwaj068@gmail.com>
2026-04-16 23:16:03 +0530

    Program Updated

---
+++ Program Practice 15-04.py
@@ -0,0 +1,115 @@
+"""
+print(10>5 and 5>2)
+print(10>20 and 50>20)
+print(10>20 and 23>12)
+
+" Not Operator"
+
+print(bool(False))
+print(bool(0))
+print(bool())
+print(bool(1))
+print(bool(-1))
+print(bool(1.1))
+print(bool("Aman"))
+
+#And for numeric it alwz gives last value as output.
+
+print(5 and 6)
+print(5 and 6 and 7 and 8)
+
+#or
+
+print(8 or 0)
+print(0 or 8)
+#not
+
+print(not(0))
+print(not(7))
+
+===================
+
+a=5
+b=10
+print(a<b<20)
+
+============
+x= True
+y=False
+print(x+y*x)
+=================
+print(4**0**2)
+
+================
+
+a=12
+b=5
+print(a^b)
+====================
+
+print((3 and 0) or (0 and 3))
+
+===========================
+
+
+a = 7 
+print(~a + 1)
+
+
+a = True 
+b = False 
+print((a or b) and not (a and b))
+
+========
+
+print(10 > 5 == True)
+================
+print(2 + 3 * 4 ** 2 // 8 % 3)
+print(1 << 2 + 1)
+
+print(3 < 5 > 2 == 2)
+print(10 // 3 * 3 + 1 % 3)
+
+x = 10 
+y = 20 
+print(x & y | x ^ y)
+
+a = True 
+b = False 
+print(a & b or a ^ b)
+
+x = 8 
+print(x >> 1 << 2)
+
+print(5 + True * False + (not False) )
+
+print((not 0) * (False or 1))
+"""
+
+
+
+
+
+
+
+print(4 + 3 * 2 ** 2 // 2 - 1) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Self Practice 12-04.py
@@ -0,0 +1,62 @@
+"""
+a=10
+b=20
+c=a+b
+print(c)
+
+print("Hello World")
+print('Hello World')
+print("Hello World")
+print('''Hello World''')
+c=340
+print("Hello",c)
+
+
+a=10
+b=20
+c=a+b
+print("Addition is : ",c)
+
+==================================
+
+a=input("Enter the value of a: ")
+b=input(" Enter the value of b: ")
+c=a+b
+print(c)
+
+=================================
+
+a=int(input("Enter the value of a: "))
+b=int(input(" Enter the value of b: "))
+c=a+b
+print(c)
+===================================
+
+a=float(input("Enter the value of a: "))
+b=float(input(" Enter the value of b: "))
+c=a+b
+print(c)
+
+==========================================
+"""
+
+BS= float(input(" Enter the Basic Salary: "))
+HRA=BS*0.2
+print("HRA is: ",HRA)
+DA=BS*0.3
+print("DA is : ",DA)
+TS= BS+HRA+DA
+print(" Total Salary: ",TS)
+
+
+
+
+
+
+
+
+
+
+
+
+
